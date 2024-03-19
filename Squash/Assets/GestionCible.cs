8d710e31a56cef5c3eb9c8e0d1b76016a9236377
Loris3153-Hou <130159079+Loris3153-Hou@users.noreply.github.com>
2024-03-19 16:09:37 -0400

    Maj squash 19/03

---
+++ Squash/Assets/GestionCible.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GestionCible : MonoBehaviour
+{
+    public GameObject cible;
+    public Text compteurTexte;
+    private int score = 0;
+    private int Niveau = 1;
+
+
+    void Start()
+    {
+        this.deplacementCubeNiv1();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("balle"))
+        {
+            score++;
+            this.UpdateCompterTexte();
+            switch (score)
+            {
+                case > 10:
+                    this.deplacementCubeNiv3();
+                    break;
+                case > 5:
+                    this.deplacementCubeNiv2();
+                    break;
+                default:
+                    this.deplacementCubeNiv1();
+                    break;
+            }
+        }
+    }
+
+    void UpdateCompterTexte()
+    {
+        if (compteurTexte != null)
+        {
+            compteurTexte.text = "Score : " + score.ToString();
+        }
+    }
+
+    void Update()
+    {
+        PlayerPrefs.SetString("score", "Score : " + score);
+    }
+
+
+    void deplacementCubeNiv1()
+    {
+
+        float actX = transform.position.x;
+        float newY = Random.Range(13, 23);
+        float newZ = Random.Range(-8, 8);
+        transform.position = new Vector3(actX, newY/10, newZ/10);
+    }
+
+    void deplacementCubeNiv2()
+    {
+
+        float actX = transform.position.x;
+        float newY = Random.Range(3, 33);
+        float newZ = Random.Range(-18, 18);
+        transform.position = new Vector3(actX, newY / 10, newZ / 10);
+    }
+
+    void deplacementCubeNiv3()
+    {
+
+        float actX = transform.position.x;
+        float newY = Random.Range(3, 33);
+        float newZ = Random.Range(-18, 18);
+        float actScaleX = transform.localScale.x;
+        float newScaleY = 4;
+        float newScaleZ = 4;
+        transform.localScale = new Vector3(actScaleX, newScaleY / 10, newScaleZ / 10);
+        transform.position = new Vector3(actX, newY / 10, newZ / 10);
+    }
+
+
+}
--- Squash/Assets/SceneChanger.cs
+++ Squash/Assets/SceneChanger.cs
@@ -4,15 +4,33 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     public int sceneMenu;
-    public int sceneJouer;
+    public int sceneEntrainementStandard;
+    public int sceneEntrainementLudique;
+    public int sceneStats;
+    public int scenePara;
 
     public void ChangeSceneMenu()
     {
         SceneManager.LoadScene(sceneMenu);
     }
 
-    public void ChangeSceneJouer()
+    public void ChangeSceneEntrainementStandard()
     {
-        SceneManager.LoadScene(sceneJouer);
+        SceneManager.LoadScene(sceneEntrainementStandard);
+    }
+
+    public void ChangeSceneEntrainementLudique()
+    {
+        SceneManager.LoadScene(sceneEntrainementLudique);
+    }
+
+    public void ChangeSceneStats()
+    {
+        SceneManager.LoadScene(sceneStats);
+    }
+
+    public void ChangeScenePara()
+    {
+        SceneManager.LoadScene(scenePara);
     }
 }
