bcd0a40da6b4780f43f2f9ba8f5912fa028cce77
MathildeBurgevin <mathilde.burgevin.etu@univ-lemans.fr>
2024-03-28 11:40:32 -0400

    Modification du changement de couleur des boutons de playlist

--- Squash/Assets/ChoixPasPlaylist.cs
+++ Squash/Assets/ChoixPasPlaylist.cs
@@ -3,13 +3,9 @@ using UnityEngine.UI;
 
 public class ChoixPasPlaylist : MonoBehaviour {
 
-    public void Click() {
-        GetComponent<Image>().color = new Color(157,157,157);
-        GameObject btn = GameObject.Find("playlist1");
-        btn.GetComponent<Image>().color = Color.white;
-        btn = GameObject.Find("playlist2");
-        btn.GetComponent<Image>().color = Color.white;
-        btn = GameObject.Find("playlist3");
-        btn.GetComponent<Image>().color = Color.white;
+    public GameObject bouton;
+
+    public void Start() {
+        bouton.SetActive(true);
     }
 }
